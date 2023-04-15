d84349e7328cad1c7d9e41010b2427586720559f
Ambika Pd Mahato <ambikamahato53@gmail.com>
2023-04-15 21:00:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee => {
+    if (employee.profession === 'developer') {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee => {
+    if (employee.profession === 'developer') {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i].profession === 'admin') {
+      arr.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr2 = [{ id: 5, name: "rock", age: "45", profession: "CEO" }, { id: 6, name: "david", age: "44", profession: "CTO" }, { id: 7, name: "kate", age: "43", profession: "manager" }];
+  const combinedArr = arr.concat(arr2);
+  console.log(combinedArr);
 }
